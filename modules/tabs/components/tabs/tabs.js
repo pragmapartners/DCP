b9db2fe71986a8541034861b82ab3c6f72b2163e
Brynn Briedis <brynn@pragma.com.au>
2024-11-29 16:11:43 +1100

    adding the basic tabs component

---
+++ modules/tabs/components/tabs/tabs.js
@@ -0,0 +1,200 @@
+/**
+ * Tabs — a lightweight JavaScript package to facilitate a11y-compliant tabbed interfaces
+ */
+export default class Tabs {
+  tabs = []
+  tabItems = null
+  panels = null
+  currentIndex = -1
+  isVerticalOrientation = false
+
+  /**
+   *
+   * @param {string} tabListSelector A CSS selector to pointing to your tablist — the parent element to your tab buttons
+   * which must have attribute `[role="tablist"]` as we will combine this selector to find all its child tab buttons with
+   * `role="tab"`. So your tab buttons must all have the `[role="tab"]` attributes. These should also contain `aria-controls`
+   * attribute pointing to the ID of corresponding tab-panel. For example:
+   *   `<div role="tablist"><button class="tab-item tab-button" role="tab" aria-controls="panel-1">Tab 1</button>...and so on`
+   * @param {string} panelsSelector A CSS selector to find by `querySelectorAll`, your panel items. This selector
+   * must be able to find all panels with role="tabpanel" items that correspond to your tab button's `aria-controls` IDs:
+   *   ```js
+   *   <div id="panel-1" role="tabpanel" tab-title="Tab 1">
+   *     <div>Tab 1 content.</div>
+   *   </div>
+   *   ```
+   * @param {number} activeIndex The index of the initial tab you'd like opened. Defaults to 0.
+   */
+  constructor(tabListSelector, panelsSelector, activeIndex) {
+    const initialActiveIndex = activeIndex || 0
+    this.tabItems = document.querySelectorAll(`${tabListSelector} [role="tab"]`)
+    this.panels = document.querySelectorAll(panelsSelector)
+    const tablist = document.querySelector(tabListSelector)
+    const orientation = tablist.getAttribute("aria-orientation")
+    this.isVerticalOrientation = orientation && orientation === "vertical" ? true : false
+    this.currentIndex = initialActiveIndex
+    this.selectTab = this.selectTab.bind(this)
+    this.deselectTabs = this.deselectTabs.bind(this)
+    this.resetPanels = this.resetPanels.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+    this.handleKeydown = this.handleKeydown.bind(this)
+    this.initTabs()
+  }
+
+  /**
+   * Only ran once from our contructor this sets up the tabs, panels, and adds click and keydown listeners
+   */
+  initTabs() {
+    this.resetPanels()
+    this.deselectTabs()
+    for (let i = 0; i < this.tabItems.length; i++) {
+      const tab = this.tabItems[i]
+      this.tabs[i] = tab
+      this.tabs[i].index = i
+      tab.addEventListener("click", this.handleClick)
+      tab.addEventListener("keydown", this.handleKeydown)
+      if (i === this.currentIndex) {
+        this._selectTab(tab)
+      }
+    }
+  }
+
+  /**
+   * Activates a panel by removing it's hidden attribute and setting aria-expanded to true
+   * @param {string} panelId a tab panel's ID obtained from the `aria-controls` attribute
+   */
+  activatePanel(panelId) {
+    document.querySelector(`#${panelId}`).removeAttribute("hidden")
+    document.querySelector(`#${panelId}`).setAttribute("aria-expanded", true)
+  }
+
+  /**
+   * Used to remove .active class from all tabs, set to tabindex -1, and aria-selected false.
+   */
+  deselectTabs() {
+    for (let i = 0; i < this.tabItems.length; i++) {
+      const tab = this.tabItems[i]
+      tab.classList.remove("active")
+      tab.setAttribute("tabindex", "-1")
+      tab.setAttribute("aria-selected", false)
+    }
+  }
+
+  /**
+   * Essentially, will set all tab panels to hidden, aria-expanded false, and tabindex 0 (so they can be tabbed into)
+   */
+  resetPanels() {
+    for (let j = 0; j < this.panels.length; j++) {
+      const panel = this.panels[j]
+      panel.setAttribute("hidden", true)
+      panel.setAttribute("aria-expanded", false)
+      panel.setAttribute("tabindex", "0")
+    }
+  }
+
+  /**
+   * Sets the selected tab and delegates to `activatePanel` to, ultimately, select a new
+   * tab / tab panel pair that is selected aka expanded.
+   *
+   * @param {HTMLElement} tabElement the tab button element to select
+   */
+  selectTab(tabElement) {
+    this.deselectTabs()
+    this.resetPanels()
+    this._selectTab(tabElement)
+  }
+
+  _selectTab(tabElement) {
+    tabElement.classList.add("active")
+    tabElement.setAttribute("aria-selected", "true")
+    tabElement.removeAttribute("tabindex")
+    this.activatePanel(tabElement.getAttribute("aria-controls"))
+  }
+
+  /**
+   *
+   * @param {Event} ev fired when user clicks or presses <space | enter> on a tab button
+   */
+  handleClick(ev) {
+    this.selectTab(ev.target)
+  }
+
+  decrementIndex() {
+    this.currentIndex--
+    if (this.currentIndex < 0) {
+      this.currentIndex = this.tabItems.length - 1
+    }
+  }
+
+  incrementIndex() {
+    this.currentIndex++
+    if (this.currentIndex >= this.tabItems.length) {
+      this.currentIndex = 0
+    }
+  }
+
+  /**
+   * This is used to implement our a11y keyboard navigation.
+   *
+   * @param {Event} ev fires when the user presses a key
+   * @returns
+   */
+  handleKeydown(ev) {
+    switch (ev.key) {
+      case "Up": // IE Edge
+      case "ArrowUp":
+        // If orientation vertical update current index else "fall-through" so default return
+        if (this.isVerticalOrientation) {
+          this.decrementIndex()
+          break
+        }
+      case "Down": // IE Edge
+      case "ArrowDown":
+        // If orientation vertical update current index else "fall-through" so default return
+        if (this.isVerticalOrientation) {
+          this.incrementIndex()
+          break
+        }
+      case "Left": // IE Edge
+      case "ArrowLeft":
+        // If orientation horizontal update current index else "fall-through" so default return
+        if (!this.isVerticalOrientation) {
+          this.decrementIndex()
+          break
+        }
+      case "Right": // IE Edge
+      case "ArrowRight":
+        // If orientation horizontal update current index else "fall-through" so default return
+        if (!this.isVerticalOrientation) {
+          this.incrementIndex()
+          break
+        }
+      case "Home":
+      case "ArrowHome":
+        this.currentIndex = 0
+        break
+      case "End":
+      case "ArrowEnd":
+        this.currentIndex = this.tabItems.length - 1
+        break
+      case "Enter":
+      case "Space":
+        this.selectTab(ev.target)
+        ev.target.focus()
+        break
+      default:
+        // If an unaccounted for key is pressed returning here prevents the
+        // `preventDefault` below (thus preserving normal behavior)
+        return
+    }
+    ev.preventDefault()
+
+    /**
+     * Ensure we have proper focus on currently selected tab. We do
+     * NOT want to set its tabindex yet though; that only happens when
+     * we click, enter, or space on a tab button.
+     */
+    this.tabs[this.currentIndex].focus()
+  }
+}
+
+window.Tabs = Tabs
---
+++ modules/tabs/src/Plugin/Block/TabsBlock.php
@@ -0,0 +1,309 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\tabs\Plugin\Block;
+
+use Drupal\Core\Block\Attribute\Block;
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\Component\Uuid\UuidInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a tabs block.
+ */
+#[Block(
+  id: 'tabs',
+  admin_label: new TranslatableMarkup('Tabs'),
+  category: new TranslatableMarkup('Pragma'),
+)]
+final class TabsBlock extends BlockBase implements ContainerFactoryPluginInterface
+{
+  /**
+   * The database connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  private Connection $connection;
+
+  /**
+   * The UUID service.
+   *
+   * @var \Drupal\Component\Uuid\UuidInterface
+   */
+  private UuidInterface $uuidService;
+
+
+  /**
+   * Constructs the plugin instance.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    Connection $connection,
+    UuidInterface $uuid_service,
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->connection = $connection;
+    $this->uuidService = $uuid_service;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): self
+  {
+    return new self(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('database'),
+      $container->get('uuid')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration(): array
+  {
+    return [
+      'unique_id' => '',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state): array
+  {
+    $form = parent::blockForm($form, $form_state);
+    $form['#tree'] = TRUE;
+
+    // Ensure 'unique_id' is set
+    if (empty($this->configuration['unique_id'])) {
+      $this->configuration['unique_id'] = $this->uuidService->generate();
+    }
+    $block_id = $this->configuration['unique_id'];
+
+    // Load existing tabs from the database.
+    $query = $this->connection->select('tabs_data', 't')
+      ->fields('t', ['id', 'title', 'content', 'format', 'icon'])
+      ->condition('block_id', $block_id)
+      ->execute();
+
+    $tabs = $query->fetchAllAssoc('id');
+
+    $form['tabs'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Tabs'),
+      '#prefix' => '<div id="tabs-wrapper">',
+      '#suffix' => '</div>',
+    ];
+
+    $i = 0;
+    foreach ($tabs as $id => $tab) {
+      $form['tabs'][$i] = [
+        '#type' => 'details',
+        '#title' => $this->t($tab->title ?? 'Tab @num', ['@num' => $i + 1]),
+        '#open' => FALSE,
+      ];
+      $form['tabs'][$i]['id'] = [
+        '#type' => 'hidden',
+        '#value' => $id,
+      ];
+      $form['tabs'][$i]['title'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Title'),
+        '#default_value' => $tab->title,
+      ];
+      $form['tabs'][$i]['icon'] = [
+        '#type' => 'icon_autocomplete',
+        '#title' => $this->t('Icon'),
+        '#default_value' => $tab->icon,
+        '#allowed_icon_pack' => [
+          'heroicons',
+        ],
+        '#show_settings' => TRUE,
+      ];
+      $form['tabs'][$i]['content'] = [
+        '#type' => 'text_format',
+        '#title' => $this->t('Content'),
+        '#default_value' => $tab->content,
+        '#format' => $tab->format ?? filter_default_format(),
+      ];
+      $i++;
+    }
+
+    // Add empty forms for new tabs if any.
+    $additional_tabs = $form_state->get('additional_tabs') ?? 0;
+    for ($j = $i; $j < $i + $additional_tabs; $j++) {
+      $form['tabs'][$j] = [
+        '#type' => 'details',
+        '#title' => $this->t('Tab @num', ['@num' => $j + 1]),
+        '#open' => TRUE,
+      ];
+      $form['tabs'][$j]['title'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Title'),
+      ];
+      $form['tabs'][$j]['icon'] = [
+        '#type' => 'icon_autocomplete',
+        '#title' => $this->t('Icon'),
+        '#allowed_icon_pack' => [
+          'heroicons',
+        ],
+        '#show_settings' => TRUE,
+      ];
+      $form['tabs'][$j]['content'] = [
+        '#type' => 'text_format',
+        '#title' => $this->t('Content'),
+        '#format' => filter_default_format(),
+      ];
+    }
+
+    $form['add_tab'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Add Tab'),
+      '#submit' => [[$this, 'addTab']],
+      '#ajax' => [
+        'callback' => [$this, 'ajaxCallback'],
+        'wrapper' => 'tabs-wrapper',
+      ],
+    ];
+
+    return $form;
+  }
+
+  public function addTab(array &$form, FormStateInterface $form_state)
+  {
+    $additional_tabs = $form_state->get('additional_tabs') ?? 0;
+    $form_state->set('additional_tabs', $additional_tabs + 1);
+    $form_state->setRebuild();
+  }
+
+  public static function ajaxCallback(array &$form, FormStateInterface $form_state)
+  {
+    return $form['settings']['tabs'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state): void
+  {
+    // Ensure 'unique_id' is set
+    if (empty($this->configuration['unique_id'])) {
+      $this->configuration['unique_id'] = $this->uuidService->generate();
+    }
+    $block_id = $this->configuration['unique_id'];
+
+    $tabs = $form_state->getValue('tabs');
+
+    // Fetch existing tab IDs for this block.
+    $existing_tab_ids = \Drupal\Core\Database\Database::getConnection()->select('tabs_data', 't')
+      ->fields('t', ['id'])
+      ->condition('block_id', $block_id)
+      ->execute()
+      ->fetchCol();
+
+    $submitted_tab_ids = [];
+
+    foreach ($tabs as $tab) {
+      if (isset($tab['id'])) {
+        $submitted_tab_ids[] = $tab['id'];
+
+        // Update existing tab.
+        \Drupal\Core\Database\Database::getConnection()->update('tabs_data')
+          ->fields([
+            'title' => $tab['title'],
+            'content' => $tab['content']['value'],
+            'format' => $tab['content']['format'],
+            'icon' => $tab['icon']['icon']->getId(),
+          ])
+          ->condition('id', $tab['id'])
+          ->execute();
+      } else {
+        // Insert new tab.
+        $insert_id = \Drupal\Core\Database\Database::getConnection()->insert('tabs_data')
+          ->fields([
+            'block_id' => $block_id,
+            'title' => $tab['title'],
+            'content' => $tab['content']['value'],
+            'format' => $tab['content']['format'],
+            'icon' => $tab['icon']['icon']->getId(),
+          ])
+          ->execute();
+        $submitted_tab_ids[] = $insert_id;
+      }
+    }
+
+    // Delete tabs that were removed.
+    $tabs_to_delete = array_diff($existing_tab_ids, $submitted_tab_ids);
+    if (!empty($tabs_to_delete)) {
+      $this->connection->delete('tabs_data')
+        ->condition('id', $tabs_to_delete, 'IN')
+        ->execute();
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(): array
+  {
+    // Ensure 'unique_id' is set
+    if (empty($this->configuration['unique_id'])) {
+      $this->configuration['unique_id'] = $this->uuidService->generate();
+    }
+    $block_id = $this->configuration['unique_id'];
+
+    // Load tabs from the database.
+    $query = $this->connection->select('tabs_data', 't')
+      ->fields('t', ['title', 'content', 'format', 'icon'])
+      ->condition('block_id', $block_id)
+      ->execute();
+
+    $tabs = [];
+    foreach ($query as $record) {
+      // $icon_details = $this->iconFinder->getIconDetails($record->icon);
+      // @todo: Implement IconFinder service to get icon details.
+      $pluginManagerIconPack = \Drupal::service('plugin.manager.icon_pack');
+
+      $tabs[] = [
+        'title' => $record->title,
+        'content' => [
+          '#type' => 'processed_text',
+          '#text' => $record->content,
+          '#format' => $record->format,
+        ],
+        'icon' => [
+          '#type' => 'icon',
+          '#pack_id' => $pluginManagerIconPack->getIcon($record->icon)->getPackId(),
+          '#icon_id' => $pluginManagerIconPack->getIcon($record->icon)->getIconId(),
+          '#settings' => [
+            'size' => 64,
+          ],
+        ],
+      ];
+    }
+    $num_tabs = count($tabs);
+
+    return [
+      '#type' => 'component',
+      '#component' => 'tabs:tabs',
+      '#props' => [
+        'items' => $tabs,
+        'tab_count' => $num_tabs,
+        'attributes' => [
+          'class' => 'tabs',
+        ],
+      ],
+    ];
+  }
+}
