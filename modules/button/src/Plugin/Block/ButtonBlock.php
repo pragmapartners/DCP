a3a789e3a7bb98adcab8a3014f80a542dee0a957
Brynn Briedis <brynn@pragma.com.au>
2024-12-23 17:11:21 +1100

    updating button

--- modules/button/src/Plugin/Block/ButtonBlock.php
+++ modules/button/src/Plugin/Block/ButtonBlock.php
@@ -2,35 +2,53 @@
 
 declare(strict_types=1);
 
-// helps tell where we are
 namespace Drupal\button\Plugin\Block;
 
-// import classes
+use Drupal\Core\Block\Attribute\Block;
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Url;
+use Drupal\Core\Template\Attribute;
 
-/**
- * Provides a button block.
- *
- * @Block(
- *   id = "dcp_button",
- *   admin_label = @Translation("Button"),
- *   category = @Translation("Pragma"),
- * )
- */
-
-
+#[Block(
+  id: 'dcp_button',
+  admin_label: new TranslatableMarkup('Button'),
+  category: new TranslatableMarkup('Pragma'),
+)]
 final class ButtonBlock extends BlockBase
 {
 
   /**
+   * Constructs a new ImageBlock instance.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
+
+  /**
    * {@inheritdoc}
    */
-  public function getCacheMaxAge()
-  {
-    return 0;
+  public static function create(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition
+  ) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+    );
   }
 
   /**
@@ -40,6 +58,13 @@ final class ButtonBlock extends BlockBase
   {
     return [
       'link' => NULL,
+      'link_title' => NULL,
+      'outline' => FALSE,
+      'size' => 'md',
+      'wide' => FALSE,
+      'disabled' => FALSE,
+      'square' => FALSE,
+      'circle' => FALSE
     ];
   }
 
@@ -48,12 +73,73 @@ final class ButtonBlock extends BlockBase
    */
   public function blockForm($form, FormStateInterface $form_state): array
   {
-    // create an entity reference field for menus
+
+    $form = parent::blockForm($form, $form_state);
+
+    $config = $this->getConfiguration();
+
     $form['link'] = [
       '#title' => $this->t('Link'),
-      '#type' => 'url',
-      '#default_value' => $this->configuration['link'],
+      '#description' => $this->t('Start typing to see a list of results. Click to select.'),
+      '#autocomplete_route_name' => 'linkit.autocomplete',
+      '#autocomplete_route_parameters' => [
+        'linkit_profile_id' => 'default',
+      ],
+      '#element_validate' => [
+        [
+          'Drupal\link\Plugin\Field\FieldWidget\LinkWidget',
+          'validateUriElement',
+        ],
+      ],
+      '#type' => 'linkit',
+      '#default_value' => $config['link'] ?? '',
       '#required' => TRUE,
+    ];
+    $form['link_title'] = [
+      '#title' => $this->t('Link Title'),
+      '#type' => 'textfield',
+      '#default_value' => $config['link_title'] ?? '',
+      '#required' => TRUE,
+    ];
+    $form['config'] = [
+      '#type' => 'details',
+      '#title' => $this->t('Button Configuration'),
+      '#open' => FALSE,
+    ];
+    $form['config']['outline'] = [
+      '#title' => $this->t('Outline'),
+      '#type' => 'checkbox',
+      '#default_value' => $config['outline'] ?? FALSE,
+    ];
+    $form['config']['size'] = [
+      '#title' => $this->t('Size'),
+      '#type' => 'select',
+      '#options' => [
+        'sm' => $this->t('Small'),
+        'md' => $this->t('Medium'),
+        'lg' => $this->t('Large'),
+      ],
+      '#default_value' => $config['size'] ?? 'md',
+    ];
+    $form['config']['wide'] = [
+      '#title' => $this->t('Wide'),
+      '#type' => 'checkbox',
+      '#default_value' => $config['wide'] ?? FALSE,
+    ];
+    $form['config']['disabled'] = [
+      '#title' => $this->t('Disabled'),
+      '#type' => 'checkbox',
+      '#default_value' => $config['disabled'] ?? FALSE,
+    ];
+    $form['config']['square'] = [
+      '#title' => $this->t('Square'),
+      '#type' => 'checkbox',
+      '#default_value' => $config['square'] ?? FALSE,
+    ];
+    $form['config']['circle'] = [
+      '#title' => $this->t('Circle'),
+      '#type' => 'checkbox',
+      '#default_value' => $config['circle'] ?? FALSE,
     ];
     return $form;
   }
@@ -63,7 +149,23 @@ final class ButtonBlock extends BlockBase
    */
   public function blockSubmit($form, FormStateInterface $form_state): void
   {
-    $this->configuration['link'] = $form_state->getValue('link');
+    $link = $form_state->getValue('link');
+    $link_title = $form_state->getValue('link_title');
+    $outline = !empty($form_state->getValue('outline')) ? $form_state->getValue('outline') : 0;
+    $size = !empty($form_state->getValue('size')) ? $form_state->getValue('size') : 'md';
+    $wide = !empty($form_state->getValue('wide')) ? $form_state->getValue('wide') : 0;
+    $disabled = !empty($form_state->getValue('disabled')) ? $form_state->getValue('disabled') : 0;
+    $square = !empty($form_state->getValue('square')) ? $form_state->getValue('square') : 0;
+    $circle = !empty($form_state->getValue('circle')) ? $form_state->getValue('circle') : 0;
+
+    $this->setConfigurationValue('link', $link);
+    $this->setConfigurationValue('link_title', $link_title);
+    $this->setConfigurationValue('outline', $outline);
+    $this->setConfigurationValue('size', $size);
+    $this->setConfigurationValue('wide', $wide);
+    $this->setConfigurationValue('disabled', $disabled);
+    $this->setConfigurationValue('square', $square);
+    $this->setConfigurationValue('circle', $circle);
   }
 
   /**
@@ -71,11 +173,38 @@ final class ButtonBlock extends BlockBase
    */
   public function build(): array
   {
+    $config = $this->getConfiguration();
+
+    $link = $config['link'] ?? '';
+    $link_title = $config['link_title'] ?? '';
+    $outline = $config['outline'] ?? FALSE;
+    $size = $config['size'] ?? 'md';
+    $wide = $config['wide'] ?? FALSE;
+    $disabled = $config['disabled'] ?? FALSE;
+    $square = $config['square'] ?? FALSE;
+    $circle = $config['circle'] ?? FALSE;
+
+    $link = \Drupal\Core\Link::fromTextAndUrl($link_title, \Drupal\Core\Url::fromUri($link));
+    $link = $link->toRenderable();
+
+    $link_attributes = new Attribute();
+
+    foreach (_build_sdc_data_props($config, ['link', 'link_title'], ['outline', 'wide', 'disabled', 'square', 'circle']) as $key => $value) {
+      $link_attributes->setAttribute($key, $value);
+    }
+
     return [
       '#type' => 'component',
       '#component' => 'button:button',
       '#props' => [
-        'link' => $this->configuration['link'],
+        'link' => $link,
+        'outline' => $outline ? TRUE : FALSE,
+        'size' => $size,
+        'wide' => $wide ? TRUE : FALSE,
+        'disabled' => $disabled ? TRUE : FALSE,
+        'square' => $square ? TRUE : FALSE,
+        'circle' => $circle ? TRUE : FALSE,
+        'attributes' => $link_attributes,
       ],
     ];
   }
